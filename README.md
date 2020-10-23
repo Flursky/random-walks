e3cb8def2e37b5d18672820e588103a4232e4173
Flursky <69195319+Flursky@users.noreply.github.com>
2020-10-23 23:02:50 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Random Walks Simulation
+
+## Tasks
+1. simulate one walks of k steps
+2. simulate n walks
+3. report average distance from origin
+
+## References
+- [MIT Course](https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0002-introduction-to-computational-thinking-and-data-science-fall-2016/index.htm)
+- [Course Video](https://www.youtube.com/watch?v=6wUD_gp5WeE)
---
+++ randomwalks.py
@@ -0,0 +1,104 @@
+import random
+
+class Location(object):
+    
+    def __init__(self, x, y):
+        """
+        Represent a location
+        Args:
+            x (float): x position
+            y (float): y position
+        """
+        self.x = x
+        self.y = y
+    
+    def move(self, deltaX, deltaY):
+        self.x += deltaX
+        self.y += deltaY
+
+        return Location(self.x, self.y)
+
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def distFrom(self, other):
+        xDist = self.x - other.getX()
+        yDist = self.y - other.getY()
+        return (xDist**2 + yDist**2)**0.5 
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+
+class Drunk(object):
+    
+    def __init__(self, name = None):
+        """
+        Base class for represent a drunk
+        Args:
+            name (str): drunk name
+        """
+        self.name = name
+    
+    def __str__(self):
+        if self.name is None:
+            return "Anonymous"
+        return self.name
+
+class UsualDrunk(Drunk):
+
+    def takeStep(self):
+        choices = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        
+        return random.choice(choices)
+
+class MasochistDrunk(Drunk):
+    
+    def takeStep(self):
+        choices = [(0.0, 1.1), (0.0, -0.9), (1.0, 0.0), (-1.0, 0.0)]
+        
+        return random.choice(choices)
+
+class Field(object):
+    
+    def __init__(self):
+        self.drunks = {}
+
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError("Drunk already exists")
+        else:
+            self.drunks[drunk] = loc
+        
+    def getLoc(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError("Drunk does not exists")
+        else:
+            return self.drunks[drunk]
+    
+    def moveDrunk(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk does not exists')
+        
+        xDist, yDist = drunk.takeStep()
+
+        self.drunks[drunk] = self.drunks[drunk].move(xDist, yDist)
+    
+def walk(f, d, numSteps):
+    """
+    Simulate a single walk
+    Args:
+        f (Field): field
+        d (Drunk): drunk
+        numSteps (integer): walk length
+    """
+
+    start = f.getLoc(d)
+
+    for step in range(numSteps):
+        f.moveDrunk(d)
+
+    return start.distFrom(f.getLoc(d))
